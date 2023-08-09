78084413b2ca59f6f118380a203366b80e4e25a5
htn9sys <hatsune.927@gmail.com>
2023-08-09 18:12:49 +0900

    フォーム入力→受け取り

---
+++ sample20/submit.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>sample20</title>
+</head>
+<body>
+  <?php if (!empty($_REQUEST['my_name'])): ?><!--もし何も入力しなかった場合-->
+    <p>お名前: <?php echo htmlspecialchars ($_REQUEST['my_name'], ENT_QUOTES); ?></p>
+  <!-- htmlspecialcharsを入れて不正もしくは危険なアクセス等を防ぐ-->
+  <?php endif; ?>
+</body>
+</html>
